0a9d46432a8ba2e18017ee65ef8a2e0ebf9c7a4d
barok21 <birukdagne99@gmail.com>
2025-07-10 16:22:14 +0300

    cleaning code

--- app/property/page.tsx
+++ app/property/page.tsx
@@ -1,14 +1,8 @@
-import OnboardingWrapper from '@/components/onboarding-wrapper'
-import InputOTPDemo from '@/components/otp'
-import { InputOTP } from '@/components/ui/input-otp'
-import UserOnboardingForm from '@/components/user-onboarding-form'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>hello
-      
-    </div>
+    <div>page</div>
   )
 }
 
